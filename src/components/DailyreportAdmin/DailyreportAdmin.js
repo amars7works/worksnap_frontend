e088001ff9e00c1575817841c137495f540ed457
jalagari sai kiran <sai@s7works.io>
2019-06-10 11:02:33 +0000

    Report download

--- src/components/DailyreportAdmin/DailyreportAdmin.js
+++ src/components/DailyreportAdmin/DailyreportAdmin.js
@@ -3,7 +3,10 @@ import axios from "axios";
 import "./DailyreportAdmin.css";
 import CustomNavigation from "../SideNavbar/SideNavbar";
 import DatePicker from "react-datepicker";
+
 import "react-datepicker/dist/react-datepicker.css";
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
 
 export class DailyreportAdmin extends Component {
   constructor(props) {
@@ -15,14 +18,11 @@ export class DailyreportAdmin extends Component {
       endDate: new Date()
     };
     this.handleClick = this.handleClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
     this.handleChangeStart = this.handleChangeStart.bind(this);
     this.handleChangeEnd = this.handleChangeEnd.bind(this);
-    this.handleChange = this.handleChange.bind(this);
   }
-
-  handleClick(currentTab) {
-    this.setState({ currentTab });
-  }
+ 
   handleChange = ({ startDate, endDate }) => {
     startDate = startDate || this.state.startDate;
     endDate = endDate || this.state.endDate;
@@ -38,6 +38,11 @@ export class DailyreportAdmin extends Component {
 
   handleChangeEnd = endDate => this.handleChange({ endDate });
 
+
+  handleClick(currentTab) {
+    this.setState({ currentTab });
+  }
+   
   componentDidMount() {
     const config = {
       url: "/api/get_requests/",
@@ -52,7 +57,28 @@ export class DailyreportAdmin extends Component {
       // console.log(res.data)
     });
   }
+
+
   render() {
+    const options = [
+      { value: 'one', label: 'One' },
+      { value: 'two', label: 'Two', className: 'myOptionClassName' },
+      {
+        type: 'group', name: 'project Name', items: [
+          { value: 'three', label: 'JVB', className: 'myOptionClassName' },
+          { value: 'four', label: 'Worksnap' }
+        ]
+      },
+      {
+        type: 'group', name: 'Employe Name', items: [
+          { value: 'five', label: 'Sai Kiran' },
+          { value: 'six', label: 'Vikram' }
+        ]
+      }
+    ];
+
+    const defaultOption = options[0];
+    
     return (
       <div>
         <CustomNavigation />
@@ -61,42 +87,58 @@ export class DailyreportAdmin extends Component {
           <div>
             <h2>Daily Report Status</h2>
             <div className="left-col">
-              <div className="filter">
-                <h4> Filters</h4>
 
-                <DatePicker
-                  className="form-control"
-                  name="range1"
-                  ref="range1"
-                  id="range1"
-                  value={this.state.data_start}
-                  placeholderText="Click to select a date"
-                  selected={this.state.startDate}
-                  selectsStart
-                  startDate={this.state.startDate}
-                  endDate={this.state.endDate}
-                  onChange={this.handleChangeStart}
-                  // dateFormat="MM/DD/YYYY"
-                />
-                <DatePicker
-                  className="form-control"
-                  ref="range"
-                  name="range"
-                  id="range"
-                  value={this.state.data_end}
-                  placeholderText="Click to select a date"
-                  selected={this.state.endDate}
-                  selectsEnd
-                  startDate={this.state.startDate}
-                  endDate={this.state.endDate}
-                  onChange={this.handleChangeEnd}
-                  //  dateFormat="MM/DD/YYYY"
-                />
-
-
-              </div>
             </div>
+            <div className="filter">
+            
+            <DatePicker
+                        className="form-control"
+                        name="range1"
+                        ref="range1"
+                        id="range1"
+                        value={this.state.data_start}
+                        placeholderText="Click to select a date"
+                        selected={this.state.startDate}
+                        selectsStart
+                        startDate={this.state.startDate}
+                        endDate={this.state.endDate}
+                        onChange={this.handleChangeStart}
+                        // dateFormat="MM/DD/YYYY"
+                      />
+                      <DatePicker
+                        className="form-control"
+                        ref="range"
+                        name="range"
+                        id="range"
+                        value={this.state.data_end}
+                        placeholderText="Click to select a date"
+                        selected={this.state.endDate}
+                        selectsEnd
+                        startDate={this.state.startDate}
+                        endDate={this.state.endDate}
+                        onChange={this.handleChangeEnd}
+                        //  dateFormat="MM/DD/YYYY"
+                      />
+            </div>
+
+
             <div className="tab">
+
+
+
+
+                <Dropdown options={options}
+                  onChange={this._onSelect}
+                  value={defaultOption}
+                  placeholder="Select an option">
+
+                </Dropdown>
+
+              
+
+
+
+
               {this.state.posts.map((post, i) => (
                 <button
                   key={post.username}
--- src/components/WorksnapReport/Worksnap_Report.js
+++ src/components/WorksnapReport/Worksnap_Report.js
@@ -1,127 +1,124 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { Input } from "reactstrap";
-import './Worksnap_Report.css';
+import "./Worksnap_Report.css";
 import axios from "axios";
 
 export class WorksnapReport extends Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props) {
-        super(props);
-    
-        this.selectOptions = this.selectOptions.bind(this);
+    this.selectOptions = this.selectOptions.bind(this);
 
-        this.state = {
-          
-         
-         selected_option: "",
-          posts:[],
+    this.state = {
+      month: "",
+      posts: [],
+      scheduled: null,
+      year:null,
+    };
+  }
 
-        };
+  handleSubmit() {
+    console.log("handle submit");
+    const year = new Date().getFullYear();
+    // console.log(year,posts)
+
+    // posts ={
+    //   month:this.state.selected_option,
+    // }
+  }
+
+  componentDidMount() {}
+
+  selectOptions(e) {
+    const year = new Date().getFullYear();
+    let value = e.target.value;
+    console.log("year is", year);
+    console.log("value is", value);
+    const config = {
+      url: "/api/user_report/",
+      method: "GET",
+      withCredentials: true,
+      params: {
+        month: value,
+        year: year,
+        user_name: "all"
       }
-
-      handleSubmit(){
-        console.log("handle submit")
-          const year = new Date().getFullYear();
-          // console.log(year,posts)
-
-          // posts ={
-          //   month:this.state.selected_option,
-          // }
-        
-        
-        }
-
-     
-    componentDidMount() {
-      
-    }
-
-
-    selectOptions(e) {
-      const year = new Date().getFullYear();
-      let value = e.target.value;
-      console.log("year is",year)
-      console.log("value is",value)
-      const config = {
-        url: "/api/user_report/",
-        method: "GET",
-        withCredentials: true,
-        params:{
-          month:value,
-          year:year,
-          user_name	:'all',
-
-        }
-        // posts:[]
-
-      };
-      axios(config).then(res => {
-        console.log("response is:",res.data)
-        this.setState({
-          posts: res.data
-        });
-  
-        // console.log(res.data)
-      });
+      // posts:[]
+    };
+    axios(config).then(res => {
+      console.log("response is:", res.data);
       this.setState({
-        selected_option: value
+        posts: res.data
       });
-    }
 
-
+      // console.log(res.data)
+    });
+    this.setState({
+      month: value,
+      year:year,
+    });
+  }
 
   render() {
     const { CustomNavigation } = this.props;
 
     return (
       <div>
-                <CustomNavigation />
-                <div className="container">
-
+        <CustomNavigation />
+        <div className="container">
           <div className="Work_snapReport">
-          <h4>Work Snap Employees Report</h4>
-          <div className="col-md-3 Work_snapReport">
+            <h4>Work Snap Employees Report</h4>
+              <div className="col-md-3 Work_snapReport">
+                <Input
+                  type="select"
+                  className="custom-select form-control"
+                  name="selected_option"
+                  value={this.state.month}
+                  ref="selected_option"
+                  id="selected_option"
+                  onChange={this.selectOptions}
+                  // onClick={this.handleSubmit}
+                >
+                  <option value="">select Month</option>
+                  <option value="1">January</option>
+                  <option value="2">February</option>
+                  <option value="3">March</option>
+                  <option value="4">April</option>
+                  <option value="5">May</option>
+                  <option value="6">June</option>
+                  <option value="7">July</option>
+                  <option value="8">August</option>
+                  <option value="9">September</option>
+                  <option value="10">October</option>
+                  <option value="11">November</option>
+                  <option value="12">December</option>
+                </Input>
+                
 
-          <Input
-                      type="select"
-                      className="custom-select"
-                      name="selected_option"
-                      value={this.state.selected_option}
-                      ref="selected_option"
-                      id="selected_option"
-                      onChange={this.selectOptions}
-                      // onClick={this.handleSubmit}
-                    >
-                      <option value="">select Month</option>
-                      <option value="1">January</option>
-                      <option value="2">February</option>
-                      <option value="3">March</option>
-                      <option value="4">April</option>
-                      <option value="5">May</option>
-                      <option value="6">June</option>
-                      <option value="7">July</option>
-                      <option value="8">August</option>
-                      <option value="9">September</option>
-                      <option value="10">October</option>
-                      <option value="11">November</option>
-                      <option value="12">December</option>
-                    </Input>
+         <div>
+              <a href={`/api/user_report?month=${this.state.month}&year=${this.state.year}&user_name=all`}>
+                <button
+                  type="submit"
+                  className="btn btn-primary download "
+                  style={{ borderRadius: "20px" }}
+                >
+                  Submit
+                </button>
+              </a>
+              </div>
+              </div>
+
+
+          </div>
         </div>
-        
-
-
-       
+        <div className="col-md-8 Report" />
       </div>
-
-        </div>
-        <div className="col-md-8 Report">
-
-       
-      </div>
-      </div>
-
-    )
+    );
   }
 }
 
-export default WorksnapReport
+export default WorksnapReport;
+
+
+
+
